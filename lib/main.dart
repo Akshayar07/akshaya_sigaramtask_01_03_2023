72d6854ffc6207eb134dd49c166d1ead6f9a4e0c
Akshaya Radha Krishnan <124199146+Akshayar07@users.noreply.github.com>
2023-03-02 01:07:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Matrix Game',
-      home: game(),
+      home: Box(),
     );
   }
 }
