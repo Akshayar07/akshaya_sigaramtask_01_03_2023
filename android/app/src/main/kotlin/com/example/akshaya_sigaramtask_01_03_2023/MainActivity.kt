54f434fb3f16253430c2be76cca0891cfa4a06ca
Akshaya Radha Krishnan <124199146+Akshayar07@users.noreply.github.com>
2023-03-02 01:05:36 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/akshaya_sigaramtask_01_03_2023/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.akshaya_sigaramtask_01_03_2023
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:akshaya_sigaramtask_01_03_2023/try.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Matrix Game',
+      home: game(),
+    );
+  }
+}
+
+
+
+class Box extends StatefulWidget {
+  @override
+  _BoxState createState() => _BoxState();
+}
+
+class _BoxState extends State<Box> {
+  int _num = 1;
+  List<int> _Clickbox = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Matrix Game',style: TextStyle(color: Colors.red),)),
+        backgroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 100),
+        child: Container(
+          child: GridView.count(
+            crossAxisCount: 4,
+            children: List.generate(16, (index) {
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _num++;
+                    if (_Clickbox.length < 2) {
+                      _Clickbox.add(index);
+                    } else {
+                      _Clickbox.removeAt(0);
+                      _Clickbox.add(index);
+                    }
+                  });
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color:
+                    (_Clickbox.contains(index)) ? Colors.red : Color(0xff00008B),
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 1,
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      (_Clickbox.indexOf(index) >= 0)
+                          ? "Box #${_num - _Clickbox.length + _Clickbox.indexOf(index)}"
+                          : "",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            }),
+          ),
+        ),
+      )
+    );
+  }
+}
